ff52e7e27a91c159e57d4e8e816cb14392367ba5
apukharev-neuralegion <alexander.pukharev@brightsec.com>
2023-04-04 02:53:03 +0300

    interviewProject - skeleton - added url check

--- cypress/pages/login-page.js
+++ cypress/pages/login-page.js
@@ -11,18 +11,21 @@ class loginPage{
         passwordInputLabel:() => cy.get('mat-label.ng-tns-c8-1 > span'),
         emailInputErrorField:() => cy.get('#mat-mdc-error-0'),
         passwordInputErrorField:() => cy.get('#mat-mdc-error-1')
-    };
+    }
+
     checkThatPageElementsAreExists(){
         this.elements.userNameInputField().should('exist');
         this.elements.passwordInputField().should('exist');
         this.elements.emailInputLabel().should('exist');
         this.elements.passwordInputLabel().should('exist');
         this.elements.signInButton().should('be.disabled').should('contain.text', 'Sign in');
-    };
+    }
+
     checkThatLoginFormElementsContainCorrectText(){
         this.elements.emailInputLabel().should('contain', 'Email');
         this.elements.passwordInputLabel().should('contain', 'Password');
-    };
+    }
+
     loginAndCheckStatusCode(userName, password, url, statusCode){
         this.elements.userNameInputField().click();
         this.elements.userNameInputField().type(userName);
@@ -34,20 +37,22 @@ class loginPage{
         cy.wait('@getData').then((interception) => {
             expect(interception.response.statusCode).to.equal(statusCode);
         });
-    };
+    }
+
     checkEmailIncorrectFormatError(userName){
         this.elements.userNameInputField().click();
         this.elements.userNameInputField().type(userName);
         this.elements.passwordInputField().click();
         this.elements.emailInputErrorField().should('have.text', 'Incorrect email format');
-    };
+    }
+
     checkThatEmailPasswordFieldsAreRequiredError(){
         this.elements.userNameInputField().click();
         this.elements.passwordInputField().click();
         this.elements.userNameInputField().click();
         this.elements.emailInputErrorField().should('have.text', 'Email is required');
         this.elements.passwordInputErrorField().should('have.text', 'Password is required');
-    };
+    }
 }
 
 module.exports = new loginPage();
--- cypress/pages/scan-table-page.js
+++ cypress/pages/scan-table-page.js
@@ -1,20 +1,19 @@
 require('cypress-xpath')
 
-
 class scanTablePage{
-
     elements ={
         scanInputLable:() => cy.get('.cdk-column-name > .title'),
     };
+
     checkThatScanTitlesContain(){
         this.elements.scanInputLable().should('contain', 'DVWA - HAR All no-smart +AO');
         this.elements.scanInputLable().should("have.length", 10);
-    };
-    checkUrlPattern(urlPart){
-        cy.url().should('include', urlPart);
-    };
-}
+    }
 
+    checkUrlPattern(urlPart){
+        cy.url().should('include', urlPart)
+    }
+}
 module.exports = new scanTablePage();
 
 
